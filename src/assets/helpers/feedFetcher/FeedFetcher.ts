166b91b80c8a84a7751dbbf939effe03a77658a3
LarsGKodehode <104063134+LarsGKodehode@users.noreply.github.com>
2022-08-10 16:27:34 +0200

    second pass (#6)
    * too much work in one commit
    
    * fixed missing update of path

---
+++ src/assets/helpers/feedFetcher/FeedFetcher.ts
@@ -0,0 +1,24 @@
+interface FeedCreateInfo {
+
+};
+
+interface FeedFetcherType {
+  
+}
+
+/**
+ * Gets a 
+ */
+const FeedFetcher: FeedFetcherType = (createInfo: FeedCreateInfo) => {
+
+
+  return {
+
+  };
+};
+
+export default FeedFetcher;
+
+export type {
+  FeedCreateInfo,
+};
